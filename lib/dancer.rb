68d45e01dcaa921326c3cb59d1973943f8136333
“zed” <zed@kolk.me>
2021-01-21 16:34:41 -0600

    commit tree 971e0abf6f22fcdd0d71dd5162bb98d5e5c22bd9

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,12 @@
-require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './class_methods_module.rb'
+class Dancer# < Dance
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
